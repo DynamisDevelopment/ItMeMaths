129c6f98011464e322c852f4a86471e7fa46fa8e
unknown <devlead@dynamisdevelopment.com>
2019-10-18 14:48:02 -0400

    Initial commit from gatsby: (https://github.com/DynamisDevelopment/gatsby-starter.git)

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Styles from '../styles/styles.sass'
+
+const Layout = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default () => Layout
