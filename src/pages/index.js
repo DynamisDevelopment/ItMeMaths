1b119dffce7890df4e2f4be76e3fdf014bdab2b8
unknown <devlead@dynamisdevelopment.com>
2019-10-30 14:32:06 -0400

    Added Slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from 'gatsby'
-import GraphImg from "graphcms-image"
-import { Carousel } from 'react-responsive-carousel'
-import Masonry from 'react-masonry-component'
+
+//* Styles 
 import '../styles.sass'
 import '../styles/index.sass'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
 // * Components 
 import Layout from '../components/layout'
+import GraphImg from "graphcms-image"
+import { CarouselProvider, Slider, Slide } from 'pure-react-carousel'
+import Masonry from 'react-masonry-component'
 
 const Home = () => {
     const data = useStaticQuery(graphql`
     query {
         slider: graphcms {
-            posts(first: 1) {
+            posts {
                 title 
                 description
                 slug 
@@ -39,20 +42,34 @@ const Home = () => {
         }
     }
     `)
+
     const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
     const largeNum = () => randomNum(200, 600)
 
+    const settings = {
+        naturalSlideWidth: 4,
+        naturalSlideHeight: 1,
+        totalSlides: data.slider.posts.length,
+        infinite: true
+    }
+
     return (
         <Layout>
-            {data.slider.posts.map((post, index) => {
-                return <div key={index} className='slide'>
-                    <div className="banner-content-front">
-                        <Link to={'posts/' + post.slug} className='title'><h1>{post.title}</h1></Link>
-                        <h3 className='desc'>{post.description}</h3>
-                    </div>
-                    <GraphImg image={post.image} withWebp={true} fit={'clip'} className='banner home-banner' />
-                </div>
-            })}
+            <CarouselProvider {...settings}>
+                <Slider>
+                    {data.slider.posts.map((post, index) => {
+                        return <Slide key={index} index={index} className='slide'>
+                            <div className="banner-content-front">
+                                <Link to={'posts/' + post.slug} className='title'>
+                                    <h1>{post.title}</h1>
+                                    <h3 className='desc'>{post.description}</h3>
+                                </Link>
+                            </div>
+                            <GraphImg image={post.image} withWebp={true} fit={'clip'} className='banner home-banner' />
+                        </Slide>
+                    })}
+                </Slider>
+            </CarouselProvider>
 
             <div className="wrapper">
                 <Masonry className={'posts'}>
