8f6efa87ea14e0551ac744fc65d6f30692d085ea
unknown <devlead@dynamisdevelopment.com>
2019-11-19 16:20:02 -0500

    Fixed rendering and sizing problem
    - Now all subCategories and Posts are listed
    - Section flush with top of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.sass'
 import { graphql, useStaticQuery, Link } from 'gatsby'
-import { animated, useTransition } from 'react-spring'
+import { animated, useTransition, useSpring } from 'react-spring'
 
 const Navbar = ({ show, toggle }) => {
     const data = useStaticQuery(graphql`
@@ -36,90 +36,43 @@ const Navbar = ({ show, toggle }) => {
         }
     }
     `)
-
     const NavbarTransition = useTransition(show, null, {
         from: { transform: 'translateX(100%)' },
         enter: { transform: 'translateX(0%)' },
         leave: { transform: 'translateX(100%)' }
     })
 
-    const [subShow, toggleSubs] = useState(false)
-    const subTransition = useTransition(subShow, null, {
-        from: { transform: 'translateX(-100%)' },
-        enter: { transform: 'translateX(0%)' },
-        leave: { transform: 'translateX(-100%)' }
-    })
-
-    const [postShow, togglePosts] = useState(false)
-    const postTransition = useTransition(postShow, null, {
-        from: { transform: 'translateX(-100%)' },
-        enter: { transform: 'translateX(0%)' },
-        leave: { transform: 'translateX(-100%)' }
-    })
+    const [showExtras, toggleExtras] = useState(false)
 
     return (
         <div>
             {NavbarTransition.map(({ item, props }) => (
                 item && <animated.div className='sidebar' style={props}>
-                    <div className="top" onClick={() => toggleSubs(false)}>
+                    <div className="top">
                         <h1>ItMeMaths</h1>
                         <h1 className='close' onClick={() => toggle(false)}>X</h1>
                     </div>
                     <h2 className="sectionTitle">Subjects</h2>
                     <ul className="categories">
                         <div className="veil"
-                            style={{ visibility: !subShow ? 'hidden' : 'visible' }}
-                            onClick={() => {
-                                toggleSubs(false)
-                                togglePosts(false)
-                            }}></div>
+                            style={{ visibility: !showExtras ? 'hidden' : 'visible' }}
+                            onClick={() => toggleExtras(false)}></div>
 
-                        {/* // * Categories */}
-                        {data.categories.categories.map((subject, key) => {
-                            return <li className="category" key={key}>
-                                <img src="../assets/icons/left-arrow.svg" className="more" onClick={() => toggleSubs(!subShow)} />
+                        {data.categories.categories.map((subject, key) => <SubCategory
+                            subject={subject} key={key}
+                            showExtras={showExtras} toggleExtras={toggleExtras} />)}
 
-                                {/* // * SubCatagories */}
-                                {subTransition.map(({ item, props, key }) => (
-                                    item && <animated.ul className="subCategories" style={props}>
-                                        {subject.subCategories.map((subCat, key) => {
-                                            return <div className="category subCat" key={key} onClick={() => postShow ? togglePosts(false) : ''}>
-                                                <img src="../assets/icons/left-arrow.svg" className="more" onClick={() => togglePosts(!postShow)} />
-                                                <div className="veil"
-                                                    style={{ visibility: !postShow ? 'hidden' : 'visible' }}
-                                                    onClick={() => togglePosts(false)}></div>
-
-                                                {/* // * Posts */}
-                                                {postTransition.map(({ item, props, key }) => (
-                                                    item && <animated.ul className="subCategories subPosts" style={props}>
-                                                        {subCat.posts.map((post, key) => {
-                                                            return <li className="post"><Link to={'posts/' + post.slug}>{post.title}</Link></li>
-                                                        })}
-                                                    </animated.ul>
-                                                ))}
-
-                                                <Link to={'archive/' + subCat.slug}>{subCat.title}</Link>
-                                            </div>
-                                        })}
-                                    </animated.ul>
-                                ))}
-
-                                <Link to={'archive/' + subject.slug}>{subject.name}</Link>
-                            </li>
-                        })}
                     </ul>
                     <h2 className="sectionTitle">Extras</h2>
                     <h2 className="sectionTitle">Misc Links</h2>
                     <h2 className="sectionTitle">Contact</h2>
-                    {
-                        data.profile.personals.map((profile, index) => {
-                            return <ul className='contact'>
-                                <li>{profile.address}</li>
-                                <li>{profile.email}</li>
-                                <li>{profile.phoneNumber}</li>
-                            </ul>
-                        })
-                    }
+                    {data.profile.personals.map((profile, index) => {
+                        return <ul className='contact'>
+                            <li>{profile.address}</li>
+                            <li>{profile.email}</li>
+                            <li>{profile.phoneNumber}</li>
+                        </ul>
+                    })}
                 </animated.div>
             ))
             }
@@ -128,3 +81,43 @@ const Navbar = ({ show, toggle }) => {
 }
 
 export default Navbar
+
+
+const SubCategory = ({ subject, key, showExtras, toggleExtras }) => {
+    const [subListShow, toggleSubs] = useState(false)
+    const subListSpring = useSpring({ transform: subListShow && showExtras ? 'translateX(0%)' : 'translateX(-100%)' })
+
+    const [postListShow, togglePosts] = useState(false)
+    const postListSpring = useSpring({ transform: postListShow && showExtras ? 'translateX(0%)' : 'translateX(-100%)' })
+
+    return (
+        <li className="category" key={key}>
+            <img src="../assets/icons/left-arrow.svg" className="more" onClick={() => {
+                toggleSubs(!subListShow)
+                toggleExtras(true)
+            }} />
+
+            {/* // * SubCatagories */}
+            <animated.ul className="subCategories" style={subListSpring} onClick={() => postListShow ? togglePosts(false) : ''}>
+                {subject.subCategories.map((subCat, key) => {
+                    return <div className="category subCat" key={key}>
+                        <img src="../assets/icons/left-arrow.svg" className="more" onClick={() => togglePosts(!postListShow)} />
+                        <div className="veil"
+                            style={{ visibility: !postListShow ? 'hidden' : 'visible' }}
+                            onClick={() => togglePosts(false)}></div>
+
+                        {/* // * Posts */}
+                        <animated.ul className="subCategories subPosts" style={postListSpring}>
+                            {subCat.posts.map((post, key) => <li className="post">
+                                <Link to={'posts/' + post.slug}>{post.title}</Link>
+                            </li>)}
+                        </animated.ul>
+                        <Link to={'archive/' + subCat.slug}>{subCat.title}</Link>
+                    </div>
+                })}
+            </animated.ul>
+
+            <Link to={'archive/' + subject.slug}>{subject.name}</Link>
+        </li>
+    )
+}
